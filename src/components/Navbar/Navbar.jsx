3a10e8695e2fb9fbb810937ffc48554238dad0e9
Nehal <mdnehaladil@gmail.com>
2023-08-12 22:00:02 +0530

    add Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  // change state acording time
+  const [hour, setHour] = useState(0);
+  useEffect(() => {
+    const date = new Date();
+    setHour(date.getHours());
+  }, []);
+
+  return (
+    <div className="Navbar mb-4 ">
+      <nav
+        className="navbar  navbar-dark justify-content-between "
+        style={{ backgroundColor: "#212529" }}
+      >
+        <Link to="/" className="navbar-brand m-2 ms-5">
+          <h1 className="text-light text-center">HABIT TRACKER</h1>
+        </Link>
+        <form action="" className="form-inline">
+          <Link
+            to="/add"
+            className="btn btn-success  me-5 my-sm-0 m-2 font-sans-serif "
+            type="submit"
+          >
+            Add New Habits
+          </Link>
+        </form>
+      </nav>
+      <h2 className="text-info text-center mt-2">
+        Good &nbsp;
+        {hour <= 12
+          ? "Morning"
+          : hour <= 17
+          ? "Afternoon"
+          : hour <= 21
+          ? "Evening"
+          : "Night"}
+      </h2>
+    </div>
+  );
+};
+
+export default Navbar;
